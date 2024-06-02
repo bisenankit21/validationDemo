e638ca5ee1ae6824aa7fd949c6beffcf55834879
AnkitBisen <bisenankit21@gmail.com>
2024-06-03 00:26:26 +0530

    Initial commit

---
+++ src/test/java/com/example/springDemo/mvc/ValidationDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springDemo.mvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ValidationDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
